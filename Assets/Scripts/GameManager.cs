181e6d971dcbe9641af7cce94d5584932068bb30
William AbouSharaf <liams_1998@hotmail.com>
2020-04-14 00:36:50 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void WinGame()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GameJam-Template
+ Unity 2019.3.0f6 custom 2D template for game jams.
