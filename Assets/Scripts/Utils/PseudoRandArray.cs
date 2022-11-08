0d8704ad23fd341f4a16a5a1a9bc143b13741c5a
WillYum <liamshere98@gmail.com>
2022-11-09 00:50:47 +0200

    Make it easy to access ArrayUtil class and functions

--- Assets/Scripts/Utils/PseudoRandArray.cs
+++ Assets/Scripts/Utils/PseudoRandArray.cs
@@ -4,81 +4,8 @@ using UnityEngine;
 
 namespace Utils.ArrayUtils
 {
-    [System.Serializable]
-    public class PseudoRandArray<T>
-    {
-        [SerializeField] private T[] _array;
-
-        public int Length { get { return _array.Length; } }
-
-        private int _index;
-
-        public PseudoRandArray(T[] array)
-        {
-            _array = array;
-            _index = 0;
-        }
-
-        public T PickNext()
-        {
-            if (_index >= _array.Length)
-            {
-                _index = 0;
-            }
-            return _array[_index++];
-        }
-    }
-
-
     public static class ArrayTools
     {
-        [System.Serializable]
-        public class PsuedoRandArray<T>
-        {
-            public T[] items;
-            private int _currentIndex = 0;
-            public PsuedoRandArray(params T[] _items)
-            {
-                if (_items == null)
-                {
-                    _items = new T[] { };
-                }
-                else
-                {
-                    items = _items;
-                }
-
-            }
-
-
-            public T PickNext()
-            {
-                T selectedItem = items[_currentIndex];
-
-                _currentIndex += 1;
-                if (_currentIndex >= items.Length)
-                {
-                    ArrayTools.ShuffleArray(items);
-                    _currentIndex = 0;
-                }
-
-                return selectedItem;
-            }
-
-            public void ShuffleArray()
-            {
-#if UNITY_EDITOR
-                if (items == null)
-                {
-                    Debug.LogError("Array is null in psuedo random array of TYPE: " + typeof(T).ToString());
-                }
-#endif
-
-                ArrayTools.ShuffleArray(items);
-            }
-        }
-
-
         public static void ShuffleList<T>(List<T> list)
         {
             for (int i = list.Count - 1; i > 0; i--)
@@ -99,6 +26,51 @@ namespace Utils.ArrayUtils
                 array[i] = array[randIndex];
                 array[randIndex] = tmp;
             }
+        }
+    }
+
+
+    [System.Serializable]
+    public class PseudoRandArray<T>
+    {
+        public T[] items;
+        private int _currentIndex = 0;
+        public PseudoRandArray(params T[] _items)
+        {
+            if (_items == null)
+            {
+                _items = new T[] { };
+            }
+            else
+            {
+                items = _items;
+            }
+        }
+
+        public T PickNext()
+        {
+            T selectedItem = items[_currentIndex];
+
+            _currentIndex += 1;
+            if (_currentIndex >= items.Length)
+            {
+                ArrayTools.ShuffleArray(items);
+                _currentIndex = 0;
+            }
+
+            return selectedItem;
+        }
+
+        public void ShuffleArray()
+        {
+#if UNITY_EDITOR
+            if (items == null)
+            {
+                Debug.LogError("Array is null in pseudo random array of TYPE: " + typeof(T).ToString());
+            }
+#endif
+
+            ArrayTools.ShuffleArray(items);
         }
     }
 }
